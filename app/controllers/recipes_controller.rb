64dc2a7fa37ab0d2ad1a36d7dfab8e076f60dc77
Oliver <olivercoimbra@gmail.com>
2022-04-19 13:07:19 -0400

    Add Food controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -57,6 +57,10 @@ class RecipesController < ApplicationController
     end
   end
 
+  def public
+    
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_recipe
