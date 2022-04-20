0987fd0bcacc80a0aaf6693a1143f72bcb73900f
Oliver <olivercoimbra@gmail.com>
2022-04-20 16:53:25 -0400

    Add form and list of foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+
+  get '/foods/new(/:recipe_id)', to: 'foods#new'
+
   resources :foods
-
-  devise_for :users
   resources :recipes
-
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
