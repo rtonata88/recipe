75e921a068fe70af05a6d2064874f7f143ae3338
Richard Tonata <rtonata@gmail.com>
2022-04-21 23:30:22 +0200

    Add home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
     def active_class(link_path)
         current_page?(link_path) ? "bg-gray-900" : ""
     end
+
+    def active_link(link_path)
+        current_page?(link_path) ? true : false
+    end
 end
